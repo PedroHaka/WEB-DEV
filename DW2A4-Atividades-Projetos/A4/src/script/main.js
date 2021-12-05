f89e4df3aeca0eec63bfa5a28666192131481323
Pedro Haka <70613448+PedroHaka@users.noreply.github.com>
2021-12-05 17:00:05 -0300

    update main.js

--- DW2A4-Atividades-Projetos/A4/src/script/main.js
+++ DW2A4-Atividades-Projetos/A4/src/script/main.js
@@ -1,17 +1,23 @@
-/* document.querySelector('form').addEventListener('submit', event => {
-    //console.log('enviar o formulario');
-    //prevenir o envio do formulário com preventDefault()
-    event.preventDefault();
-})  */
-
 import masks from './modules/mask.js';
+import validations from './modules/valid.js';
 
-document.querySelectorAll('input').forEach(($input) => {
-    const fields = $input.dataset.js;
+function init() {
+    document.querySelectorAll('input').forEach(($input) => {
+        const fields = $input.dataset.js;
 
-    $input.addEventListener('input', (e) => {
-        e.target.value = masks[fields](e.target.value);
-    }, false);
-});
+        $input.addEventListener('input', (e) => {
+            e.target.value = masks[fields](e.target.value);
+            let validStatus = validations[fields](e.target.value);
+            if(validStatus == true){
+                document.getElementById(`input_${[fields]}`).classList.add('validInput');
+            }
+            if(validStatus == false){
+                document.getElementById(`input_${[fields]}`).classList.add('errorInput');
+            }
+        }, false);
+    });
+}
+
+init();
 
 
