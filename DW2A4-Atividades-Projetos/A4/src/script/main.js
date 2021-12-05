02b8b1cdf77b2cc3b5544584fa5b7fa714a6c4b9
Pedro Haka <70613448+PedroHaka@users.noreply.github.com>
2021-12-05 14:47:03 -0300

    Delete main.js

--- DW2A4-Atividades-Projetos/A4/src/script/main.js
+++
@@ -1,6 +0,0 @@
-document.querySelector('form').addEventListener('submit', event => {
-    console.log('enviar o formulario');
-    //prevenir o envio do formulário com preventDefault()
-    event.preventDefault();
-})
-
